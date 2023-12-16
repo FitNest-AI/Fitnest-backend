9bec6daf3e2140faaa6699a989cd20f051be1c20
Zaidan syahrani <zdnsyrhn22@gmail.com>
2023-12-17 05:49:08 +0700

    Update seeder.js

--- seeder.js
+++ seeder.js
@@ -9,8 +9,6 @@ const GoalsModel = require('./models/GoalsModel');
 const LevelsModel = require('./models/LevelsModel');
 const DietPrefsModel = require('./models/DietPrefsModel');
 const TargetMusclesModel = require('./models/TargetMusclesModel');
-const ConditionsModel = require('./models/ConditionsModel');
-const SidesModel = require('./models/SidesModel');
 const ExercisesModel = require('./models/ExercisesModel');
 
 const populateModels = async (data) => {
@@ -43,140 +41,129 @@ const seeding = async () => {
   try {
     // Database connection Config
     await require('./configs/DatabaseConfig').connect(process.env.MONGO_URI);
-    const data = [
-      {
-        Model: RolesModels,
-        data: [
-          {
-            name: 'standard',
-          },
-          {
-            name: 'premium',
-          },
-        ],
-      },
+    // const data = [
+    //   {
+    //     Model: RolesModels,
+    //     data: [
+    //       {
+    //         name: 'standard',
+    //       },
+    //       {
+    //         name: 'premium',
+    //       },
+    //     ],
+    //   },
 
-      {
-        Model: GoalsModel,
-        data: [
-          {
-            name: 'gain muscle',
-          },
-          {
-            name: 'lose weight',
-          },
-          {
-            name: 'keep fit',
-          },
-          {
-            name: 'improving posture',
-          },
-        ],
-      },
+    //   {
+    //     Model: GoalsModel,
+    //     data: [
+    //       {
+    //         name: 'gain muscle',
+    //       },
+    //       {
+    //         name: 'lose weight',
+    //       },
+    //       {
+    //         name: 'keep fit',
+    //       },
+    //       {
+    //         name: 'improving posture',
+    //       },
+    //     ],
+    //   },
 
-      {
-        Model: LevelsModel,
-        data: [
-          {
-            name: 'newbie',
-          },
-          {
-            name: 'beginner',
-          },
-          {
-            name: 'intermediate',
-          },
-          {
-            name: 'advanced',
-          },
-        ],
-      },
+    //   {
+    //     Model: LevelsModel,
+    //     data: [
+    //       {
+    //         name: 'easy',
+    //       },
+    //       {
+    //         name: 'medium',
+    //       },
+    //       {
+    //         name: 'hard',
+    //       },
+    //     ],
+    //   },
 
-      {
-        Model: ConditionsModel,
-        data: [
-          {
-            name: 'not fit',
-          },
-          {
-            name: 'fit',
-          },
-          {
-            name: 'very fit',
-          },
-        ],
-      },
+    //   {
+    //     Model: DietPrefsModel,
+    //     data: [
+    //       {
+    //         name: 'non vegan',
+    //         desc: 'Consumes all types of food, including meat, fish, animal products, and plant-based foods',
+    //       },
+    //       {
+    //         name: 'vegan',
+    //         desc: 'Does not consume meat but may include non-meat animal products such as dairy or eggs in their diet',
+    //       },
+    //     ],
+    //   },
 
-      {
-        Model: DietPrefsModel,
-        data: [
-          {
-            name: 'flexitarian',
-            desc: 'Consumes all types of food, including meat, fish, animal products, and plant-based foods',
-          },
-          {
-            name: 'vegan',
-            desc: 'Does not consume animal products, including meat, dairy, eggs, and other animal-derived products',
-          },
-          {
-            name: 'vegetarian',
-            desc: 'Does not consume meat but may include non-meat animal products such as dairy or eggs in their diet',
-          },
-          {
-            name: 'pescetarian',
-            desc: 'Avoids meat except for fish and fish products',
-          },
-        ],
-      },
+    //   {
+    //     Model: TargetMusclesModel,
+    //     data: [
+    //       {
+    //         name: 'legs',
+    //       },
+    //       {
+    //         name: 'triceps',
+    //       },
+    //       {
+    //         name: 'glutes',
+    //       },
+    //       {
+    //         name: 'chest',
+    //       },
+    //       {
+    //         name: 'back',
+    //       },
+    //       {
+    //         name: 'abs',
+    //       },
+    //       {
+    //         name: 'shoulders',
+    //       },
+    //       {
+    //         name: 'biceps',
+    //       },
+    //       {
+    //         name: 'hamstrings',
+    //       },
+    //     ],
+    //   },
 
-      {
-        Model: TargetMusclesModel,
-        data: [
-          {
-            name: 'legs',
-          },
-          {
-            name: 'triceps',
-          },
-          {
-            name: 'glutes',
-          },
-          {
-            name: 'chest',
-          },
-          {
-            name: 'back',
-          },
-          {
-            name: 'abs',
-          },
-          {
-            name: 'shoulders',
-          },
-          {
-            name: 'biceps',
-          },
-          {
-            name: 'hamstrings',
-          },
-        ],
-      },
+    //   {
+    //     Model: SidesModel,
+    //     data: [
+    //       {
+    //         name: 'front',
+    //         instruction: 'front instruction',
+    //       },
+    //       {
+    //         name: 'side',
+    //         instruction: 'side instruction',
+    //       },
+    //     ],
+    //   },
 
-      {
-        Model: SidesModel,
-        data: [
-          {
-            name: 'front',
-            instruction: 'front instruction',
-          },
-          {
-            name: 'side',
-            instruction: 'side instruction',
-          },
-        ],
-      },
-    ];
-    await populateModels(data);
+    //   // {
+    //   //   Model: ConditionsModel,
+    //   //   data: [
+    //   //     {
+    //   //       name: 'not fit',
+    //   //     },
+    //   //     {
+    //   //       name: 'fit',
+    //   //     },
+    //   //     {
+    //   //       name: 'very fit',
+    //   //     },
+    //   //   ],
+    //   // },
+    // ];
+    // await populateModels(data);
 
     const exerciseData = [
       {
@@ -186,101 +173,79 @@ const seeding = async () => {
             name: 'squat',
             desc: 'Squat is a fundamental lower body exercise that targets the quadriceps, hamstrings, and glutes. It also engages the core muscles for stability.',
             image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 3),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'landscape',
+            levelId: '657e033a7744a82ac69b8e92',
+            targetMuscleId: ['657e033a7744a82ac69b8ea1', '657e033a7744a82ac69b8e9b', '657e08b3b985a016868c769f'],
+            direction: 'side',
+            orientation: 'potrait',
             instruction: 'Stand with your feet shoulder-width apart. Lower your body by bending your knees and hips, as if sitting back into a chair. Keep your back straight and chest up. Rise back up to the starting position.',
+            start: {
+              right_knee: 0,
+              left_knee: 0,
+              right_hip: 130,
+              left_hip: 130,
+              right_elbow: 360,
+              left_elbow: 360,
+            },
+            end: {
+              right_knee: 90,
+              left_knee: 90,
+              right_hip: 50,
+              left_hip: 50,
+              right_elbow: 360,
+              left_elbow: 360,
+            },
           },
           {
             name: 'push-up',
             desc: 'Push-up is a compound exercise that targets the chest, shoulders, and triceps. It also engages the core for stability.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 1),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'portrait',
+            image: 'https://storage.googleapis.com/fitnest-project-bucket/exercise/exercise.png',
+            levelId: '657e033a7744a82ac69b8e92',
+            targetMuscleId: ['657e033a7744a82ac69b8e9c', '657e033a7744a82ac69b8e9f', '657e033a7744a82ac69b8e9a'],
+            direction: 'side',
+            orientation: 'landscape',
             instruction: 'Start in a plank position with your hands placed slightly wider than shoulder-width apart. Lower your body by bending your elbows, keeping your body in a straight line. Push back up to the starting position.',
+            start: {
+              right_knee: 10,
+              left_knee: 10,
+              right_hip: 10,
+              left_hip: 10,
+              right_elbow: 350,
+              left_elbow: 350,
+            },
+            end: {
+              right_knee: 15,
+              left_knee: 15,
+              right_hip: 10,
+              left_hip: 10,
+              right_elbow: 250,
+              left_elbow: 250,
+            },
           },
           {
-            name: 'pull-up',
-            desc: 'Pull-up is a challenging upper body exercise that targets the back, biceps, and shoulders. It helps build strength and muscle definition in the upper body.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 2),
-            sideId: await getDocId(SidesModel, 1),
+            name: 'sit-up',
+            desc: 'Sit-up is an abdominal exercise that targets the rectus abdominis and strengthens the core muscles. It is effective for building abdominal strength and endurance.',
+            image: 'https://storage.googleapis.com/fitnest-project-bucket/exercise/exercise.png',
+            levelId: '657e033a7744a82ac69b8e92',
+            targetMuscleId: ['657e033a7744a82ac69b8e9e'],
+            direction: 'side',
             orientation: 'landscape',
-            instruction: 'Hang from a pull-up bar with your palms facing away. Pull your body up towards the bar, bringing your chest close to it. Lower yourself back down with control.',
-          },
-          {
-            name: 'weighted dips',
-            desc: 'Weighted dips are an advanced upper body exercise that targets the chest, triceps, shoulders, and upper back. It involves using additional weight for added resistance.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 5),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'portrait',
-            instruction: 'Position yourself between parallel bars. Lower your body by bending your elbows until your shoulders are below your elbows. Push back up to the starting position.',
-          },
-          {
-            name: 'crunches',
-            desc: 'Crunches are a basic abdominal exercise that targets the rectus abdominis. It is effective for building core strength and definition.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 1),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'portrait',
-            instruction: 'Lie on your back with your knees bent. Place your hands behind your head and lift your upper body towards your knees. Lower back down to the starting position.',
-          },
-          {
-            name: 'shoulder press',
-            desc: 'Shoulder press is a compound exercise that targets the shoulders and triceps. It helps build upper body strength and shoulder definition.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 3),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'landscape',
-            instruction: 'Sit or stand with a weight in each hand. Lift the weights overhead by extending your arms. Lower them back down to shoulder level.',
-          },
-          {
-            name: 'Romanian deadlift',
-            desc: 'Romanian deadlift is an advanced exercise that targets the hamstrings, glutes, and lower back. It involves hip hinge movement with a straight back.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 4),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'portrait',
-            instruction: 'Hold a barbell in front of your thighs with a shoulder-width grip. Hinge at your hips, keeping your back straight, and lower the barbell towards the ground. Stand back up by extending your hips.',
-          },
-          {
-            name: 'lunges',
-            desc: 'Lunges are a basic lower body exercise that targets the quadriceps, hamstrings, and glutes. They also engage the core for balance.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 1),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'landscape',
-            instruction: 'Take a step forward with one foot and lower your body until both knees are bent at a 90-degree angle. Push back up to the starting position and repeat on the other leg.',
-          },
-          {
-            name: 'tricep dips',
-            desc: 'Tricep dips are a beginner-friendly exercise that targets the triceps. It involves lowering and raising your body using a stable surface.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 1),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'portrait',
-            instruction: 'Position yourself on parallel bars. Lower your body by bending your elbows until your upper arms are parallel to the ground. Push back up to the starting position.',
-          },
-          {
-            name: 'barbell rows',
-            desc: 'Barbell rows are an advanced compound exercise that targets the back, biceps, and shoulders. It involves pulling a barbell towards your lower chest.',
-            image: 'https://storage.googleapis.com/formal-outpost-402813-bucket/exercise/dYjuOFZi3Gu7b1O2dC5AFj6DjeYuak54-20231114-014533',
-            levelId: await getDocId(LevelsModel, 1),
-            targetMuscleId: await getDocId(TargetMusclesModel, 1),
-            sideId: await getDocId(SidesModel, 1),
-            orientation: 'landscape',
-            instruction: 'Bend at the hips and knees to pick up a barbell with a shoulder-width grip. Pull the barbell towards your lower chest, squeezing your shoulder blades together. Lower the barbell back down with control.',
+            instruction: 'Lie on your back with your knees bent and feet flat on the ground. Cross your arms over your chest or place your hands behind your ears. Engage your core as you lift your upper body toward your knees. Exhale as you come up and inhale as you lower back down to the starting position.',
+            start: {
+              right_knee: 90,
+              left_knee: 90,
+              right_hip: 130,
+              left_hip: 130,
+              right_elbow: 360,
+              left_elbow: 360,
+            },
+            end: {
+              right_knee: 90,
+              left_knee: 90,
+              right_hip: 50,
+              left_hip: 50,
+              right_elbow: 360,
+              left_elbow: 360,
+            },
           },
         ],
       },
